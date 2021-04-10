f1ab537890268507c5a4e64b65c8d5a27ac1945a
panameral <78920352+panameral@users.noreply.github.com>
2021-04-10 17:18:35 +0200

    Update using Dictionary instead of List.

--- Crittografia.py
+++ Crittografia.py
@@ -1,15 +1,25 @@
 from cryptography.fernet import Fernet
 from sys import exit
+from os.path import isfile
 
-class Crittografia():
+def genera_chiave_fernet():
+    return Fernet.generate_key()
+
+class Crittografia:
     def __init__(self, key):
         self.key = Fernet(key)
 
-    def critta(self, messaggio):
-        mj = " ".join(messaggio)
-        mc = self.key.encrypt(mj.encode())
+    def critta(self, passwords):
+        lista_temp = []
+        for plat in passwords.keys():           #questo ciclo serve per trasformare il dizionario in una lista "lineare", in modo da poterla poi trasformare
+            lista_temp.append(plat)             #in una stringa da poter criptare e scrivere su file
+            for up in passwords[plat]:
+                lista_temp.append(up)
+
+        mj = " ".join(lista_temp)
+        mc = self.key.encrypt(mj.encode())      #cripta il "messaggio" contenente piattaforme, user e password usando la libreria Fernet
         try:
-            with open('password', 'wb') as fw:
+            with open('password', 'wb') as fw:      #trascrive il "messaggio" criptato
                 fw.write(mc)
         except:
             exit("» C'è stato qualche problema con la scrittura del file delle password!")
@@ -17,11 +27,25 @@ class Crittografia():
     def decritta(self):
         mc = ''
         mf = ''
+        list_temp = []
+        up = ['', '']
+        key = ''
+        passwords = {}
+
         try:
             with open('password', 'rb') as fr:
                 mf = fr.read()
+
             mc = self.key.decrypt(mf)
             messaggio = mc.decode()
-            return messaggio.split(" ")
+            list_temp = messaggio.split(" ")         #Usando la libreria Fernet, trasforma il messaggio criptato preso dal file 'password' e lo trasforma in una lista
+
+            for i in range(0, len(list_temp), 3):    #Trasforma la lista di prima in un dizionario su cui poter lavorare in modo più efficiente
+                key = list_temp[i]
+                up[0] = list_temp[i+1]
+                up[1] = list_temp[i+2]
+                passwords.update({key: up})
+
+            return passwords
         except:
             exit("» C'è stato qualche problema con la lettura del file delle password!")
