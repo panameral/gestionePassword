1fc4f97426729a9c75064094f40d8e17a1edcff3
panameral <78920352+panameral@users.noreply.github.com>
2021-04-10 17:20:00 +0200

    Update using dictionary instead of list.
    Some comment for readability (in Italian)

--- GestisciPassword.py
+++ GestisciPassword.py
@@ -1,139 +1,132 @@
-from string import ascii_letters
-from string import digits
-from string import punctuation
+from Funzioni import pulisci_schermo
+from Crittografia import Crittografia, genera_chiave_fernet
+from os.path import isfile
+from string import ascii_letters, digits, punctuation
 from random import choice
-from ListaPass import ListaPass
-from os import system
 
+
+def generaPassword():
+    caratteri = ascii_letters + digits          #Insieme di tutte le lettere e tutti i numeri (alfabeto inglese)
+    simboli = caratteri + punctuation           #Qui oltre alle lettere e ai numeri di prima, si aggiungono una serie di simboli da poter scegliere per 
+                                                    #la generazione casuale della password
+
+    scelta = input("1. Password alfanumerica\n2. Password alfanumerica con simboli\nInserire scelta: ")
+    numero = input("Inserire numero caratteri: ")
+    
+    p = ''
+    if scelta == "1":             
+        for i in range(int(numero)+1):
+            p += choice(caratteri)
+    elif scelta == "2":      
+        for i in range(int(numero)+1):
+            p += choice(simboli)
+    
+    return p
+
+def carica_chiave():
+    if isfile("./key"):                 #Se il file della chiave esiste, lo apre semplicemente e lo ritorna come istanza, altrimenti prima genera il file e
+        try:                            #poi lo ritorna comunque come istanza
+            with open('key', 'rb') as fileK:
+                return fileK.read()
+        except:
+            exit("» C'è stato qualche problema con la lettura della chiave")
+    else:       
+        #Generazione del file della chiave
+        try:
+            with open('key', 'wb') as fileK:
+                chiave_fernet = genera_chiave_fernet()
+                fileK.write(chiave_fernet)
+        except:
+            exit("» C'è stato qualche problema con la generazione della chiave")
+                
+        #Apertura del file della chiave
+        try:
+            with open('key', 'rb') as fileK:
+                return fileK.read()
+        except:
+            exit("» C'è stato qualche problema con la lettura della chiave")
+        
 class GestisciPassword:
-    def __init__(self, key):
-        self.lp = ListaPass(key)
+    def __init__(self):
+        self.crypto = Crittografia(carica_chiave())
 
     def listaPiattaforme(self, passwords):
         print("Le piattaforme sono:")
-        for i in range(len(passwords)):
-            if i%3 == 0 or i == 0:
-                print(f"-> {passwords[i]}")
-    
-    def generaPassword(self):
-        caratteri = ascii_letters + digits
-        simboli = caratteri + punctuation
-        scelta = input("1. Password alfanumerica\n2. Password alfanumerica con simboli\nInserire scelta: ")
-        numero = input("Inserire numero caratteri: ")
-        system("clear")
-        print("La password da te richiesta è:")
-        if scelta == "1":             
-            for i in range(int(numero)):
-                print(choice(caratteri), end='') 
-        elif scelta == "2":      
-            for i in range(int(numero)):
-                print(choice(simboli), end='')
+        for platform in passwords.keys():           #Le piattaforme sono come nome delle chiavi del dizionario "passwords"
+            print(f"-> {platform}")
 
     def modifica(self, passwords):
         plat = input("Quale piattaforma? ")
         cerca = plat.lower()
         if cerca in passwords:
-            indice = passwords.index(cerca)
-            cerca = input("User (u) o Password (p)? ").lower()
-            if cerca == "u":
-                passwords[indice+1] = input("Inserisci Username nuovo: ")
-            elif cerca == "p":
-                caratteri = ascii_letters + digits
-                simboli = caratteri + punctuation
-                scelta = input("1. Password alfanumerica\n2. Password alfanumerica con simboli\nInserire scelta: ")
-                numero = input("Inserire numero caratteri: ")
-                if scelta == "1":             
-                    for i in range(int(numero)):
-                        p += choice(caratteri)
-                elif scelta == "2":      
-                    for i in range(int(numero)):
-                        p += choice(simboli)
-                passwords[indice+2] = p
-                self.lp.scriviPassword(passwords)
+            up = passwords[cerca]                                   #Essendo che la piattaforma è la chiave l'user e la password sono una lista di due elementi 
+            scelta = input("User (u) o Password (p)? ").lower()         #impostati come valore del dizionario, allora prendo il valore della chiave e lavoro su di esso
+            if scelta == "u":
+                up[0] = input("Inserisci Username nuovo: ")
+            elif scelta == "p":
+                up[1] = generaPassword()
+            passwords.update({cerca: up})
         else:
-            system("clear")
+            pulisci_schermo()
             print(f"» {plat} non risulta nel database!")
         return passwords
 
-    def leggi(self, passwords):
+    def leggi(self, passwords):                                 #Stessa impostazione del metodo "modifica"
         plat = input("Quale piattaforma? ")
         cerca = plat.lower()
         if cerca in passwords:
-            indice = passwords.index(cerca)
-            cerca = input("User (u) o Password (p)? ")
-            system("clear")
-            if cerca == "u":
-                print("L'username richiesto è:")
-                print(passwords[indice+1])
-            elif cerca == "p":
-                print("La password richiesta è:")
-                print(passwords[indice+2])
+            up = passwords[cerca]
+            print("L'username richiesto è: " + up[0])
+            print("La password richiesta è: " + up[1])
         else:
-            system("clear")
+            pulisci_schermo()
             print(f"» {plat} non risulta nel database!")
-
-    def crea(self):
-        passwords = ['', '', '']
-        plat = input("» Non c'è memorizzata nessuna password! Memorizzane una!\nQuale piattaforma? ")
-        passwords[0] = plat.lower()
-        passwords[1] = input("Inserisci Username nuovo: ")
-        caratteri = ascii_letters + digits
-        simboli = caratteri + punctuation
-        system("clear")
-        print("La password verrà generata automaticamente, scegli però alcune opzioni")
-        scelta = input("1. Password alfanumerica\n2. Password alfanumerica con simboli\nInserire scelta: ")
-        numero = input("Inserire numero caratteri: ")
-        password = ''
-        if scelta == "1":             
-            for i in range(int(numero)):
-                password += choice(caratteri)
-        elif scelta == "2":      
-            for i in range(int(numero)):
-                password += choice(simboli)
-        passwords[2] = password
-        self.lp.scriviPassword(passwords)
-        system("clear")
-        return passwords
     
-    def aggiungi(self, passwords):
-        plat = input("Quale piattaforma? ")
-        piattaforma = plat.lower()
-        passwords.append(piattaforma)
+    def aggiungi(self, passwords):                      
+        plat = input("Quale piattaforma? ").lower()
         user = input("Inserisci Username nuovo: ")
-        passwords.append(user)
-        caratteri = ascii_letters + digits
-        simboli = caratteri + punctuation
-        system("clear")
-        scelta = input("1. Password alfanumerica\n2. Password alfanumerica con simboli\nInserire scelta: ")
-        numero = input("Inserire numero caratteri: ")
-        password = ''
-        if scelta == "1":             
-            for i in range(int(numero)):
-                password += choice(caratteri)
-        elif scelta == "2":      
-            for i in range(int(numero)):
-                password += choice(simboli)
-        passwords.append(password)
-        self.lp.scriviPassword(passwords)
-        system("clear")
+
+        pulisci_schermo()
+        p = generaPassword()
+        pulisci_schermo()
+
+        list_temp = [user, p]                   #questa lista racchiude user e password che saranno impostati come valore del dizionario,
+        passwords.update({plat: list_temp})     #la cui chiave sarà la piattaforma legata ad essi
+
         return passwords
 
     def elimina(self, passwords):
         plat = input("Quale piattaforma? ")
         cerca = plat.lower()
         if cerca in passwords:
-            indice = passwords.index(cerca)
-            passwords.pop(indice)
-            passwords.pop(indice)
-            passwords.pop(indice)
-            system("clear")
+            passwords.pop(cerca)
+            pulisci_schermo()
             print(f"» I dati di {plat} sono stati eliminati!")
-            self.lp.scriviPassword(passwords)
             return passwords
         else:
-            system("clear")
+            pulisci_schermo()
             print(f"» {plat} non risulta nel database!")
             return passwords
 
-    def listaPasswords(self):
-        return self.lp.leggiPassword()
+    def carica(self):
+        if isfile("./password"):                 #Se il file delle password esiste, lo apre semplicemente e ritorna le password come lista
+            return self.crypto.decritta()       #altrimenti crea la lista
+        else:
+            #Generazione della lista delle password
+            passwords = {}
+            plat = input("» Non c'è memorizzata nessuna password! Memorizzane una!\nQuale piattaforma? ").lower()
+            user = input("Inserisci Username nuovo: ")
+
+            pulisci_schermo()
+
+            print("La password verrà generata automaticamente, scegli però alcune opzioni")
+            #Processo di generazione automatico della password
+            p = generaPassword()
+
+            list_temp = [user, p]
+            passwords.update({plat: list_temp})
+
+            return passwords
+    
+    def salva(self, passwords):
+        self.crypto.critta(passwords)
