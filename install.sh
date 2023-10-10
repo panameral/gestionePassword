ddb3318e017a9cec5f0818ef706d1a18680090a2
Stefano <78920352+stefanolod@users.noreply.github.com>
2023-10-10 09:40:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 mkdir ~/.gestionePassword
 cp ./* ~/.gestionePassword
 echo 'current_folder=$(pwd) 
-<<<<<<< HEAD
 cd ~ 
 python ~/.gestionePassword/Main.py 
-=======
 cd ~/.gestionePassword/Main.py 
 python ./Main.py 
->>>>>>> 531bd17359e56783fc16f82e055a4d9191493fbe
 cd $pwd' > pypass
 sudo mv pypass /bin/pypass
